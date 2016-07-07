a4386eefb556e4b4228062272eb2ce27cd2b5b28
Impuls3iv <sammy.unterman@gmail.com>
2016-07-06 20:07:43 -0400

    working on adding O logic

--- main.js
+++ main.js
@@ -20,8 +20,53 @@ $(document).ready(function() {
   }); //ends topL mousedown function
 
   $('#topL').mouseup(function() {
-    Math.floor((Math.random() * 9) + 1);
-    if ('#topL')
+    var randomNum = Math.floor((Math.random() * 9) + 1);
+    console.log(randomNum)
+    if (randomNum === 1){
+      //don't place O, because there is X here already
+    } else if (randomNum === 2) {
+        if ($('#topM').hasClass("clicked")){
+          //don't place O because either there is X here or O here
+        } else { //place an O in the div and give it clicked id
+          $('<p>O</p>').appendTo('#topM');
+          $('#topM').addClass("clicked");
+        }; //end nested if
+    } else if (randomNum === 3) {
+        if ($('#topR').hasClass("clicked")){
+          //don't place O because either there is X here or O here
+        } else { //place an O in the div and give it clicked id
+          $('<p>O</p>').appendTo('#topR');
+          $('#topR').addClass("clicked");
+        };//end of nested if
+    } else if (randomNum === 4) {
+        if ($('#midL').hasClass("clicked")){
+          //don't place O because either there is X here or O here
+        } else { //place an O in the div and give it clicked id
+          $('<p>O</p>').appendTo('#midL');
+          $('#midL').addClass("clicked");
+        };//end of nested if
+    } else if (randomNum === 5) {
+        if ($('#midM').hasClass("clicked")){
+          //don't place O because either there is X here or O here
+        } else { //place an O in the div and give it clicked id
+          $('<p>O</p>').appendTo('#midM');
+          $('#midM').addClass("clicked");
+        };//end of nested if
+    } else if (randomNum === 6) {
+        if ($('#midR').hasClass("clicked")){
+          //don't place O because either there is X here or O here
+        } else { //place an O in the div and give it clicked id
+          $('<p>O</p>').appendTo('#midR');
+          $('#midR').addClass("clicked");
+        };//end of nested if
+    } else if (randomNum === 5) {
+        if ($('#midM').hasClass("clicked")){
+          //don't place O because either there is X here or O here
+        } else { //place an O in the div and give it clicked id
+          $('<p>O</p>').appendTo('#midM');
+          $('#midM').addClass("clicked");
+        };//end of nested if
+    };//end of if statement
     //get random num between 1 and 9
     //check to see if any div already has a class of checked
     //if it does do the random number again
